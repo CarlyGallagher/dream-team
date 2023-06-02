5462d6c6f843e85c55072fc6080b4f44545044a7
Lamm9 <harrylamm@icloud.com>
2023-06-01 19:09:39 -0700

    added isTaken query for listed items

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,12 +16,19 @@ const resolvers = {
       }
       throw new AuthenticationError("You need to be logged in!");
     },
+    followers: async (parent, { username }) => {
+      return User.findOne({ username }).populate("followers");
+    },
     listings: async () => {
       return Listing.find();
     },
     listing: async (parent, { _id }) => {
       const listing = Listing.findOne({ _id });
       return listing;
+    },
+    listingIsTaken: async (parent, { _id }) => {
+      const listing = Listing.findOne({ _id });
+      return listing.isTaken;
     },
     reviews: async () => {
       return Review.find();
